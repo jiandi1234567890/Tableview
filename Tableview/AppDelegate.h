f1740dd226c5ba9722ef27212411f19346ab120f
jiandi1234567890 <550392461@qq,com>
2016-03-08 08:24:12 +0800

    Initial Commit

---
+++ Tableview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tableview
+//
+//  Created by 张海禄 on 16/3/8.
+//  Copyright © 2016年 张海禄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
